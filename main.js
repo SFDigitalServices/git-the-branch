67138c4cf44365f550c82f1aaa94837b8caf1b81
Shawn Allen <shawnbot@users.noreply.github.com>
2020-09-24 09:03:43 -0600

    use nodejs

---
+++ main.js
@@ -0,0 +1,26 @@
+const { getInput, exportVariable, setOutput } = require('@actions/core')
+const { spawnSync } = require('child_process')
+
+const git = (cmd, args = [], opts = {}) => {
+  return spawnSync('git', [cmd, ...args], {
+    stdio: 'inherit',
+    encoding: 'utf8'
+    ...opts
+  }).stdout.trim()
+}
+
+git('fetch')
+
+const { GITHUB_SHA } = process.env
+const nameRev = git('name-rev', ['--name-only', GITHUB_SHA])
+const branch = nameRev.replace('remotes/origin/', '')
+const ref = `refs/heads/${branch}`
+
+setOutput('branch', branch)
+setOutput('ref', ref)
+exportVariable('GIT_BRANCH', branch)
+exportVariable('GITHUB_REF', ref)
+
+if (getInput('checkout') === true) {
+  git('checkout', [branch])
+}
